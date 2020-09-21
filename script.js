a0bd8af2a4d3b671188c36dbb68b7455b607e009
leminhthanh142 <thanhle12345789@gmail.com>
2020-09-22 05:44:35 +0700

    fix the code

--- script.js
+++ script.js
@@ -1,101 +1,93 @@
-
-
 const progressBar = document.querySelector(".fill");
+let audios = document.getElementsByClassName("audio");
 let nowPlaying = document.getElementById("now-playing");
-let songName = document.querySelectorAll(".song-name");
-let audios = document.querySelectorAll(".audio");
-let song = document.querySelectorAll(".song");
+let songName = Array.from(document.querySelectorAll(".song-name"));
+let songs = Array.from(document.querySelectorAll(".song"));
 let playBtn = document.querySelector(".play-pause");
 let backBtn = document.getElementById("backBtn");
 let nextBtn = document.getElementById("nextBtn");
 let appendSec = document.getElementById("sec");
 let appendMin = document.getElementById("min");
-// let currentSong = 0;
 
+let currentSong = 0;
+let audio = new Audio();
 
+// play the song
+function playSong () {
+    audio.src = audios[currentSong].src;
+    audio.play();
+}
 
+// toggle the play - pause button
+playBtn.addEventListener("click", togglePlayPause);
+function togglePlayPause () {
+    if (audio.paused) {
+        audio.play();
+        playBtn.innerHTML = '<i class="fa fa-pause">'
+    }
+    else {
+        audio.pause();
+        playBtn.innerHTML = '<i class="fa fa-play">'
+    }
+}
 
-// convert the real time into  progress - bar
-song.forEach((song, index) => {
-    song.addEventListener("click", function () {
+audio.addEventListener("timeupdate", function () {
+    let position = audio.currentTime / audio.duration;
+    progressBar.style.width = position * 100 + "%";
 
-        // change the button appearance when user click a song
-        playBtn.innerHTML = '<i class="fa fa-pause">';
-
-
-        // play the song
-        function playSong () {
-            audios[index].play();
+    // convert the duration of song into real time with progressbar
+    convertTime();
+    function convertTime(seconds) {
+        seconds = Math.round(audio.currentTime);
+        let min = Math.floor(seconds / 60);
+        let sec = seconds % 60;
+        if (sec < 10) {
+            appendSec.innerHTML = "0" + sec;
+        } else {
+            appendSec.innerHTML = sec;
         }
-        playSong();
-
-        // click to play next song
-        nextBtn.addEventListener("click", function (){
-            index++;
-            console.log(audios[index]);
-            console.log(index);
-            playBtn.innerHTML = '<i class="fa fa-pause">'
-            if (index > audios.length) {
-                index = 0;
-            }
-            changeName();
-            playSong();
-        })
-
-
-        // click to play previous song
-        backBtn.addEventListener("click", function () {
-            index--;
-            console.log(index);
-            playBtn.innerHTML = '<i class="fa fa-pause">'
-            if (index < 0) {
-                index = audios.length;
-            }
-            changeName();
-            playSong();
-        })
-
-        // // toggle play and pause button
-        // if (audios[index].paused) {
-        //     audios[index].play();
-        //     playBtn.innerHTML = '<i class="fa fa-pause">'
-        // }
-        // else {
-        //     audios[index].pause();
-        //     playBtn.innerHTML = '<i class="fa fa-play">'
-        // }
-
-
-        // change the name of song when user click
-        function changeName () {
-            nowPlaying.innerHTML = songName[index].innerHTML;
+        if (min < 10) {
+            appendMin.innerHTML = "0" + min;
+        } else {
+            appendMin.innerHTML = min;
         }
-        changeName();
-
-
-
-        audios[index].addEventListener("timeupdate", function () {
-            // convert the duration of song into real time with progressbar
-            let position = audios[index].currentTime/audios[index].duration;
-            progressBar.style.width = position * 100 + "%";
-            convertTime();
-            function convertTime(seconds) {
-                seconds = Math.round(audios[index].currentTime);
-                let min = Math.floor(seconds/60);
-                let sec = seconds % 60;
-                if (sec < 10) {
-                    appendSec.innerHTML = "0" + sec;
-                }
-                else {
-                    appendSec.innerHTML = sec;
-                }
-                if (min < 10) {
-                    appendMin.innerHTML = "0" + min;
-                }
-                else {
-                    appendMin.innerHTML = min;
-                }
-            }
-        })
-    })
+    }
 })
+
+
+// click to play the next song
+nextBtn.addEventListener("click", function () {
+    currentSong ++;
+    console.log(currentSong);
+    playBtn.innerHTML = '<i class="fa fa-pause">';
+    if (currentSong === audios.length) {
+        currentSong = 0;
+    }
+    playSong();
+    togglePlayPause();
+    changeName();
+})
+// click to play the previous song
+backBtn.addEventListener("click", function () {
+    currentSong --;
+    playBtn.innerHTML = '<i class="fa fa-play">';
+    if (currentSong < 0) {
+        currentSong = audios.length - 1;
+    }
+    playSong();
+    togglePlayPause();
+    changeName();
+})
+
+
+// change the name of the song whenever user click a new song, next or back button
+function changeName () {
+    nowPlaying.innerHTML = songName[currentSong].innerHTML;
+}
+
+// whenever user click a song ---> play it
+songs.forEach(song => song.addEventListener("click", function () {
+    playSong();
+    togglePlayPause();
+    changeName();
+}))
